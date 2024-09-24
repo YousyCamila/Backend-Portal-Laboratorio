057bb3574fcf50634ad1b13fb403fe5dd40b77d1
YousyCamila <DANNACAMILAGUERRARINCON0@gmail.com>
2024-09-24 11:15:25 -0500

    feat(back-end): Add logic to Persona

---
+++ logicPersona/profesionalLogic.js
@@ -0,0 +1,58 @@
+// profesionalController.js
+const Profesional = require('../modelsPersona/profesionalModels');
+
+// Crear un nuevo profesional
+async function crearProfesional(body) {
+    const profesionalExistente = await Profesional.findOne({ email: body.email });
+
+    if (profesionalExistente) {
+        throw new Error('El profesional ya existe con ese email.');
+    }
+
+    const profesional = new Profesional(body);
+    return await profesional.save();
+}
+
+// Obtener todos los profesionales
+async function obtenerProfesionales() {
+    return await Profesional.find();
+}
+
+// Obtener un profesional por email
+async function obtenerProfesionalPorEmail(email) {
+    const profesional = await Profesional.findOne({ email });
+
+    if (!profesional) {
+        throw new Error('Profesional no encontrado');
+    }
+    return profesional;
+}
+
+// Actualizar un profesional por email
+async function actualizarProfesional(email, body) {
+    const profesional = await Profesional.findOneAndUpdate({ email }, body, { new: true });
+
+    if (!profesional) {
+        throw new Error('Profesional no encontrado');
+    }
+    return profesional;
+}
+
+// Eliminar un profesional por email
+async function eliminarProfesional(email) {
+    const profesional = await Profesional.findOneAndDelete({ email });
+
+    if (!profesional) {
+        throw new Error('Profesional no encontrado');
+    }
+    return { message: 'Profesional eliminado' };
+}
+
+module.exports = {
+    crearProfesional,
+    obtenerProfesionales,
+    obtenerProfesionalPorEmail,
+    actualizarProfesional,
+    eliminarProfesional,
+};
+
