7824ac0751ed99528c254f7e5e7c92bcd59d22c4
YousyCamila <DANNACAMILAGUERRARINCON0@gmail.com>
2024-09-25 11:08:18 -0500

    feat(backend): Add authentification

--- modelsPersona/usuarioModel.js
+++ modelsPersona/usuarioModel.js
@@ -1,18 +1,35 @@
-// models/User.js
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    tipoIdentificacion: {
-        type: String,
-        enum: ['Cédula de ciudadanía', 'Tarjeta de identidad', 'Pasaporte'],
-        required: true
-    },
-    numeroIdentificacion: { type: String, required: true, unique: true },
-    fechaNacimiento: { type: Date, required: true },
-    password: { type: String, required: true },
-    activo: { type: Boolean, default: true },
+  tipoIdentificacion: { 
+    type: String, 
+    enum: ['Cédula de ciudadanía', 'Tarjeta de identidad', 'Pasaporte'], 
+    required: true 
+  },
+  numeroIdentificacion: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  fechaNacimiento: { 
+    type: Date, 
+    required: true 
+  },
+  password: { 
+    type: String, 
+    required: true 
+  },
+  username: { // Asegúrate de que esta propiedad exista
+    type: String,
+    required: true,
+    unique: true,
+  },
+  activo: { 
+    type: Boolean, 
+    default: true 
+  },
 }, {
-    timestamps: true,
+  timestamps: true,
 });
 
 module.exports = mongoose.model('User', UserSchema);
